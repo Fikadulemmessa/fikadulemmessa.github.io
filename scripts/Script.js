8b6e156de2355d669a8e19b7ecc0428d903020e4
Fikadulemmessa <32723023+Fikadulemmessa@users.noreply.github.com>
2017-10-12 14:43:40 -0700

    Add files via upload

---
+++ scripts/Script.js
@@ -0,0 +1,6 @@
+"<ul id='menu'>" +
+"<li><a href='basic.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='Maps.html'>Maps</a></li>" +
+"<li><a href='Contact.html'>Contact</a></li>" +
+"</ul>";
